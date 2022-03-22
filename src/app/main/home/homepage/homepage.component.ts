86533ba1674c741eeb80a15be0594284cd864c08
Saragam Ghimire <codewithghimire@gmail.com>
2022-03-22 22:16:16 +0545

    cart number increased

--- src/app/main/home/homepage/homepage.component.ts
+++ src/app/main/home/homepage/homepage.component.ts
@@ -8,100 +8,105 @@ import { UserService } from 'src/app/user/service/user.service';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
 })
 export class HomepageComponent implements OnInit {
   rooms: any;
   premium: any;
   urgent: any;
   normal: any;
-  normalLoading:boolean;
-  urgentLoading:boolean;
-  premiumLoading:boolean;
-  cart :any;
-  cartRoom:any
+  normalLoading: boolean;
+  urgentLoading: boolean;
+  premiumLoading: boolean;
+  cart: any;
+  cartRoom: any;
+  cartNumber: number;
 
   constructor(
-    public roomService:RoomService,
-    public router:Router,
-    public msgService:MsgService,
-    public userService:UserService
-  ) { 
-    this.rooms= {}
-    this.premium={}
-    this.urgent={}
-    this.normal={}
+    public roomService: RoomService,
+    public router: Router,
+    public msgService: MsgService,
+    public userService: UserService
+  ) {
+    this.rooms = {};
+    this.premium = {};
+    this.urgent = {};
+    this.normal = {};
     this.normalLoading = false;
     this.urgentLoading = false;
-    this.premiumLoading =  false;
-    this.cartRoom = []
-
-
-    this.cart = localStorage.getItem('cart')
+    this.premiumLoading = false;
+    this.cartRoom = [];
+    if (localStorage.getItem('cartNumber')) {
+      this.cartNumber = Number(localStorage.getItem('cartNumber')!);
+    } else {
+      this.cartNumber = 0;
+    }
+    console.log('cartNumber in constructor:', this.cartNumber);
+    this.cart = localStorage.getItem('cart');
   }
 
   ngOnInit(): void {
     this.roomService.getByPremium().subscribe(
-      (data:any)=>{
-        this.premium =data
-        this.premiumLoading= true;
-        console.log('homepage categoriers',data)
+      (data: any) => {
+        this.premium = data;
+        this.premiumLoading = true;
+        console.log('homepage categoriers', data);
+      },
+      (err: any) => {
+        console.log('err', err);
       }
-      ,(err:any)=>{
-        console.log('err',err);
-      }
-    )
+    );
     this.roomService.getByUrgent().subscribe(
-      (data:any)=>{
+      (data: any) => {
         this.urgentLoading = true;
-        this.urgent =data
-        console.log('homepage categoriers',data)
-      },(err:any)=>{
-        console.log('err',err);
+        this.urgent = data;
+        console.log('homepage categoriers', data);
+      },
+      (err: any) => {
+        console.log('err', err);
       }
-    )
-        this.roomService.getByNormal().subscribe(
-      (data:any)=>{
+    );
+    this.roomService.getByNormal().subscribe(
+      (data: any) => {
         this.normalLoading = true;
-        this.normal =data
-        console.log('homepage categoriers',data)
-      },(err:any)=>{
-        console.log('err',err);
+        this.normal = data;
+        console.log('homepage categoriers', data);
+      },
+      (err: any) => {
+        console.log('err', err);
       }
-    )
-   
+    );
   }
 
-
-
-  addTocart(room:any){
+  addTocart(room: any) {
     // console.log('cart:',this.cart)
-    if(this.cart){
+    if (this.cart) {
       const roomsId = this.cart.split(', ');
-      let check:boolean = true;
-      for(let i=0;i<roomsId.length;i++){
-        if(roomsId[i] == room._id){
-          check = false
+      let check: boolean = true;
+      for (let i = 0; i < roomsId.length; i++) {
+        if (roomsId[i] == room._id) {
+          check = false;
         }
-       }      
-       if(check){
-        this.cart = this.cart + ', '+room._id;
+      }
+      if (check) {
+        this.cart = this.cart + ', ' + room._id;
         // console.log('cart:',this.cart)
-        JSON.stringify(localStorage.setItem('cart',this.cart))
+        this.cartNumber++;
+        JSON.stringify(localStorage.setItem('cart', this.cart));
         // this.getCartId(localStorage.getItem('cart'));
-       }else{
-         this.msgService.showInfo('Youhave already added this room')
-       }
-
-    }
-    else{
+      } else {
+        this.msgService.showInfo('Youhave already added this room');
+      }
+    } else {
       // console.log('room:',room)
+      this.cartNumber = 1;
       this.cart = room._id;
       // console.log("append cart:",this.cart)
-      JSON.stringify(localStorage.setItem('cart',this.cart))
+      JSON.stringify(localStorage.setItem('cart', this.cart));
     }
     // const cartRooomssss = this.getCartId()
     // console.log('haha:',cartRooomssss)
+    localStorage.setItem('cartNumber', String(this.cartNumber));
   }
 
   // getCartId(){
@@ -114,7 +119,7 @@ export class HomepageComponent implements OnInit {
   //       (data:any)=>{
   //         this.cartRoom.push(data)
   //       }
-        
+
   //     ),(err:any)=>{
   //      console.log('err',err)
   //     }
@@ -122,13 +127,13 @@ export class HomepageComponent implements OnInit {
   //   }else{
   //     this.msgService.showInfo('You have not addedroom to your cart')
   //   }
-   
+
   // return this.cartRoom
   // }
 
-  booking(){
-    this.router.navigate(['/auth/register'])
-    this.msgService.showInfo('You Have TO Register! ')
+  booking() {
+    this.router.navigate(['/auth/register']);
+    this.msgService.showInfo('You Have TO Register! ');
   }
 
   customOptions: OwlOptions = {
@@ -141,26 +146,21 @@ export class HomepageComponent implements OnInit {
     navText: ['', ''],
     responsive: {
       0: {
-        items: 1
+        items: 1,
       },
       400: {
-        items: 2
+        items: 2,
       },
       740: {
-        items: 3
+        items: 3,
       },
       940: {
-        items: 4
+        items: 4,
       },
       1080: {
-        items: 5
-      }
+        items: 5,
+      },
     },
-    nav: true
-  }
+    nav: true,
+  };
 }
-
-
-
-
-
